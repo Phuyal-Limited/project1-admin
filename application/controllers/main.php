5c8e2bcc46ecc2e26d871f0df28c67ffd457301a
Niranjan <youthniranjan@gmail.com>
2013-11-10 15:15:02 +0545

    main.php
    image upload issue 10

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -140,7 +140,7 @@ class Main extends CI_Controller {
 						
 						$ext_name = $ext[1];
 						
-						$dir = 'images/';
+						$dir = 'assests/images/book_image/';
 						
 						$tempname = $_FILES['image']['tmp_name'];
 						$size = $_FILES['image']['size'];
