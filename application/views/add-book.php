0829715e5bc2e8e09983e2003d004cbe07a93e00
bibekkc <dothemuzic@gmail.com>
2013-12-12 16:50:40 +0545

    improved the add book form

--- application/views/add-book.php
+++ application/views/add-book.php
@@ -23,27 +23,27 @@
                             <form action="<?php echo base_url();?>publish" method="post" id="publish" name="publish">
                         <fieldset style="width:48%; float:left; margin-right: 3%;"> 
                           <label>ISBN 10 *<div style="color:green;"><i class="fa fa-check"></i></div><span style="float:right; margin-right:5px;" id="load_isbn10"></span></label>
-                          <input type="text" name="isbn10" id="isbn10" placeholder="0123456789" style="width:92%;">
+                          <input type="text" name="isbn10" id="isbn10" placeholder="0123456789" style="width:89%;">
                         </fieldset>
                                     <fieldset style="width:48%; float:left;">
                           <label>ISBN 13 *<span style="float:right; margin-right:5px;" id="load_isbn13"></span></label>
-                          <input type="text" name="isbn13" id="isbn13" placeholder="978-1413304541" style="width:92%;">
+                          <input type="text" name="isbn13" id="isbn13" placeholder="978-1413304541" style="width:89%;">
                         </fieldset><div class="clear"></div>
-                                    <fieldset>
+                          <fieldset style="width:99%;">
                           <label>Book Title *</label>
-                          <input type="text" name="book_name" id="book_name">
+                          <input type="text" name="book_name" id="book_name" style="width:95%;">
                         </fieldset>
                         <fieldset style="width:48%; float:left; margin-right: 3%;">
                           <label>Author *</label>
-                          <input type="text" name="author" id="author" style="width:92%;">
+                          <input type="text" name="author" id="author" style="width:89%;">
                         </fieldset>
                         <fieldset style="width:48%; float:left;">
                           <label>Publisher *</label>
-                          <input type="text" name="publisher" id="publisher" style="width:92%;">
+                          <input type="text" name="publisher" id="publisher" style="width:89%;">
                         </fieldset>
                         <fieldset style="width:48%; float:left; margin-right: 3%;">
                           <label>Year Of Publication (A.D) * </label>
-                          <select name="published_date" id="published_date" style="width:92%">
+                          <select name="published_date" id="published_date" style="width:89%">
                             <option value="Please Choose..">Please Choose..</option>
                             <?php 
                               for($x=1910;$x<2014;$x++){
@@ -56,26 +56,26 @@
                         </fieldset>
                         <fieldset style="width:48%; float:left;">
                           <label>Edition *</label>
-                          <input type="text" name="edition" id="edition" style="width:92%;">
+                          <input type="text" name="edition" id="edition" style="width:89%;">
                         </fieldset>
                         <div class="clear"></div>
                         <fieldset style="width:48%; float:left; margin-right: 3%;">
                           <label>Language *</label>
-                          <input type="text" name="language" id="language" style="width:92%;">
+                          <input type="text" name="language" id="language" style="width:89%;">
                         </fieldset>
                         <fieldset style="width:48%; float:left;"> 
                           <label>Price *</label>
-                          <input type="text" name="price" id="price" style="width:92%;">
+                          <input type="text" name="price" id="price" style="width:89%;">
                         </fieldset>
                         <div class="clear"></div>
                         <fieldset style="width:48%; float:left; margin-right: 3%;"> 
                           <label>Weight</label>
-                          <input type="text" name="weight" id="weight" style="width:92%;">
+                          <input type="text" name="weight" id="weight" style="width:89%;">
                         </fieldset>      
                         <fieldset style="width:48%; float:left;"> 
                           <label>Category *</label>
                           <div class="category-add">
-                            <select name="category_id" id="category_id" style="width:92% !important;" class="multiselect" multiple="multiple">
+                            <select name="category_id" id="category_id" style="width:89% !important;" class="multiselect" multiple="multiple">
                             <?php 
                               for($i=0;$i<sizeof($category[0]);$i++){
                             ?>
@@ -89,26 +89,26 @@
                         <div class="clear"></div>
                         <fieldset style="width:48%; float:left; margin-right: 3%;"> 
                           <label>Delivery Cost Within City</label>
-                          <input type="text" name="delivery_cost_within_city" id="delivery_cost_within_city" style="width:92%;">
+                          <input type="text" name="delivery_cost_within_city" id="delivery_cost_within_city" style="width:89%;">
                         </fieldset>
                         <fieldset style="width:48%; float:left;"> 
                           <label>Delivery Cost Out Of City</label>
-                          <input type="text" name="delivery_cost_outof_city" id="delivery_cost_outof_city" style="width:92%;">
+                          <input type="text" name="delivery_cost_outof_city" id="delivery_cost_outof_city" style="width:89%;">
                         </fieldset>
                         <div class="clear"></div>
                         <fieldset style="width:48%; float:right;"> 
                           <label>Store Item Reference</label>
-                          <input type="text" name="store_ref" id="store_ref" style="width:92%;">
+                          <input type="text" name="store_ref" id="store_ref" style="width:89%;">
                         </fieldset>
                         <fieldset style="width:48%; float:left;"> 
                           <label>Image</label>&nbsp;
-                          <input type="file" name="image" id="image"  onChange="readURL(this);" style="width:92%;">
+                          <input type="file" name="image" id="image"  onChange="readURL(this);" style="width:89%;">
                           <div id="image_set"><div>
                           <br /><img  style="display:none;" id="img_prev" src="#" alt="Image Preview" />
                                   </fieldset>
                                     <fieldset style="width:48%; float:right;"> 
                           <label>Quantity *</label>
-                          <input type="text" name="qty" id="qty" style="width:92%;" />
+                          <input type="text" name="qty" id="qty" style="width:89%;" />
                         </fieldset>
                                     <div class="clear"></div>
                                     <fieldset>
